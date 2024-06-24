827478b5f1a2bf261180d66c7115b3958c29fc13
ONJOFTECHNOLOGIES11 <eonjoro36@gmail.com>
2024-06-24 13:22:13 +0300

    Add files via upload
    My first php project

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2024. Developed by Emmanuel Onjoro : IT-DICT/4453258/24.
+<br/>
+Online Voting System.
+<br/>
+Kitale National Politechnic. 
+<br/>
+Contact: <a href="tel:+254793048318">+254 793 048 318</a> | Email: <a href="mailto:eonjoro36@gmail.com">eonjoro36@gmail.com</a> Website: <a href="my portfolio\index.html">My Portfolio</a>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include ('head.php');?>
+
+<body>
+	<?php include ('view_banner.php');?>
+
+    <heading class="voters_heading">
+        <center><h1>KITALE NATIONAL POLITECHNIC STUDENT POLITECHNICS</h1>
+    </heading>
+    <?php include 'slider.php';?>
+    <div class="image">
+    	<img src="img/img1.jpg" width="40%" hieght="40%"/>
+    </div>
+    <div class="union-infor">
+    	More Information about us...
+    </div>
+
+
+    <?php    
+        include ('footer.php');
+        ?>
+
+   </body>
+</html>
+
